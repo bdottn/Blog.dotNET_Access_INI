8c53dc897cffc7bc0557a638409a2e9987d75ee7
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-05 15:18:46 +0800

    建立 INIAccessTests 單元測試
    1.　加入測試案例：GetValueTest_傳入section為WebSite_傳入key為Google_預期回傳Google網址

---
+++ PhysicalUnitTest/INIAccessTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Physical.UnitTest
+{
+    [TestClass]
+    public sealed class INIAccessTests
+    {
+        private INIAccess access;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Lab.ini");
+
+            this.access = new INIAccess(path);
+        }
+
+        [TestMethod]
+        public void GetValueTest_傳入section為WebSite_傳入key為Google_預期回傳Google網址()
+        {
+            var section = "WebSite";
+
+            var key = "Google";
+
+            var expected = @"https://www.google.com/";
+
+            var actual = access.GetValue(section, key);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
