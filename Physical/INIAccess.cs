129a5b15d04186c8096bb5d94388e624f8abb66e
Echo Lee <bdottn.lee+git@gmail.com>
2017-07-05 15:17:34 +0800

    建立 INIAccess
    1.　引用 dll：kernel32
    　1.1　加入外部方法：GetPrivateProfileString
    
    2.　加入方法：GetValue

---
+++ Physical/INIAccess.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Physical
+{
+    public sealed class INIAccess
+    {
+        private readonly string path;
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(
+            string lpAppName,
+            string lpKeyName,
+            string lpDefault,
+            StringBuilder lpReturnedString,
+            int nSize,
+            string lpFileName);
+
+        public INIAccess(string path)
+        {
+            this.path = path;
+        }
+
+        public string GetValue(string section, string key)
+        {
+            var tmpStringBuilder = new StringBuilder(255);
+
+            GetPrivateProfileString(section, key, string.Empty, tmpStringBuilder, 255, this.path);
+
+            var value = tmpStringBuilder.ToString();
+
+            return value;
+        }
+    }
+}
